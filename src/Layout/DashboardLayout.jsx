d253d280ad03ed5bf21de27d95c1688bbdf34ecb
Abdullah Javaid <abdullahjaved4504@gmail.com>
2023-09-26 11:30:09 +0500

    abc

--- src/Layout/DashboardLayout.jsx
+++ src/Layout/DashboardLayout.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
-import Home from '../componentsDashboard/Dashboard/Home';
 import Navbar from '../componentsDashboard/global/Navbar';
 import Collections from "../componentsDashboard/Dashboard/Collections/Collections";
 import ListCollections from "../componentsDashboard/Dashboard/Collections/ListCollections";
 import ListOrders from "../componentsDashboard/Dashboard/Orders/ListOrders";
 import ListCustomers from "../componentsDashboard/Dashboard/Customers/ListCustomers";
-import Articles from "../components/Home";
 
 
 
@@ -16,7 +14,6 @@ export default function DashboardLayout() {
         <Box sx={{ display: 'flex' }}>
             <Navbar />
             <Routes>
-                <Route path='/articles' element={<Articles/>}></Route>
                 <Route path='/collections' element={<Collections />}></Route>
                 <Route path='/collections/list' element={<ListCollections/>}></Route>
                 <Route path='/collections/orders' element={<ListOrders/>}></Route>
